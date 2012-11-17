606c3fd2d487835cb6fde28d15822c632447baa6
Remy <plespagnol@sfr.fr>
2012-11-17 18:59:39 +0100

    maj error

--- errors/page-404.php
+++ errors/page-404.php
@@ -1,5 +1,5 @@
 <article>
   <h2>Erreur 404 : Introuvable — Not Found</h2>
   <h3>Oups ! La ressource spécifiée n’existe pas.</h3>
-  <p>Si le problème persite, vous pouvez m’envoyer un courriel à <a href="mailto:ensilan[at]ensisa.info"><code>ensilan[at]ensisa.info</code></a>.</p>
+  <p>Si le problème persite, vous pouvez nous envoyer un courriel à <a href="mailto:xid[at]ensisa.info"><code>xid[at]ensisa.info</code></a>.</p>
 </article>

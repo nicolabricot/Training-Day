a99566b859e18459cb8df1cbd711727ce3ae89e6
nicolabricot <github@nicolabricot.com>
2012-11-17 18:23:21 +0100

    màj design

--- pages/infos.php
+++ pages/infos.php
@@ -6,6 +6,41 @@
 
 <section>
     <article>
+        <h3>Quand ?</h2>
+        <p>
+        La prochaine LAN se déroulera le 22 novembre 2012 de 8h à 19h à l’ENSISA Lumière !<br />
+        <a href="#">Inscrivez-vous dès maintenant !</a>
+        </p>
+        <address>
+        École Nationale Supérieure d'Ingénieurs Sud-Alsace<br />
+        12 Rue des Frères Lumière<br />
+        68093 Mulhouse<br />
+        Tél : 03 89 33 69 00
+        </address>
+    </article>
+    <article>
+        <h3>Où ?</h3>
+        <script>
+          map = '<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"'+
+                'src="https://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=fr&amp;geocode=&amp;q=ensisa&amp;sll=4'+
+                '7.729221,7.310715&amp;sspn=0.007751,0.021136&amp;t=h&amp;gl=fr&amp;ie=UTF8&amp;hq=ensisa&amp;hnear'+
+                '=&amp;ll=47.729221,7.310715&amp;spn=0.007751,0.021136&amp;output=embed"></iframe>';
+          document.write(map);
+        </script>
+    </article>
+    <article>
+        <h3>Quoi d’autre ?</h3>
+        <ul>
+            <li>Possibilité de se restaurer à la K'fet Lumière (Pizza, boisson, ...)</li>
+            <li>Début des tournois à 19h, venez en avance si vous voulez avoir le temps de vous installer tranquillement</li>
+            <li>De nombreuses animations tout au long de la soirée !</li>
+            <li>Pour vous inscrire dès maintenant, cliquez <a href="#">ici</a></li>
+        </ul>
+    </article>
+</section>
+<!--
+<section>
+    <article>
         <h3>EnsiLAN, c’est...</h3>
         <ul>
             <li>Une interface web permattant de gérer les LANs organisés à l’ENSISA.</li>
@@ -34,3 +69,4 @@
         </p>
     </article>
 </section>
+-->

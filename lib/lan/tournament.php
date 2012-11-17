9561285e33ad483e9e1ea54bd97a5d7747cc48bf
Karl Woditsch <k.woditsch@gmail.com>
2012-11-17 13:39:36 +0100

    tournament

---
+++ lib/lan/tournament.php
@@ -0,0 +1,69 @@
+<?php
+namespace lib\user;
+use lib\db\DataBase;
+/**
+ * @author Karl
+ */
+class Tournament {
+    private $id = 0;
+	private $name;
+	private $description;
+	private array $schedules;
+	private $game;
+
+	public __construct((Game) $game){
+		$this->game = $game;
+	}
+
+	public hydrate(array $datas){
+		foreach($datas as $key => $value){
+            switch($key){
+                case 'id':
+                    $this->$key = (int) $value;
+                    break;
+                case 'name':
+                case 'description':
+                    $this->$key = (string) $value;
+                    break;
+            }
+        }
+	}
+
+	public function addSchedule($label, Schedule $schedule){
+		$this->schedules[$label] = $schedule;
+	}
+	public function getSchedules(){
+		return $this->schedules;
+	}
+	public function rmSchedule($label){
+		unlink($this->schedules[$label]);
+	}
+	public function getName(){
+		return $this->name;
+	}
+	public function setName($str){
+		$this->name = (string) $str;
+	}
+	public function getDescription(){
+		return $this->name;
+	}
+	public function setDescription($str){
+		$this->name = (string) $str;
+	}
+	public function getGame(){
+		return $this->game;
+	}
+
+	public function countPlayers(){
+		return count($this->players);
+	}
+
+	public static function countTournaments(){
+	    $req = DataBase::getInstance()->prepare('SELECT COUNT(id) FROM tournament');
+		$req->execute();
+		$count = $req->fetchColumn();
+		$req->closeCursor();
+		return $count;
+	}
+}
+?>

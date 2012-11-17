fb530d7e834c052ad429d90aca83af4669f3f05c
Karl Woditsch <k.woditsch@gmail.com>
2012-11-17 09:45:03 +0100

    Création de l'index, spl & namespaces

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+namespace lib;
+
+spl_autoload_extensions('.php');
+spl_autoload_register();
+
+session_start();
+
+//Template maitre, les pages supplémentaires sont à mettre dans le dossier pages/
+
+
+
+
+
+
+?>

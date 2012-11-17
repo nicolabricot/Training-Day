f2a6392d3daac5831f284ee2614e176b1333b474
nicolabricot <github@nicolabricot.com>
2012-11-17 16:37:43 +0100

    Ajout page 404

---
+++ errors/page-404.php
@@ -0,0 +1,5 @@
+<article>
+  <h2>Erreur 404 : Introuvable — Not Found</h2>
+  <h3>Oups ! La ressource spécifiée n’existe pas.</h3>
+  <p>Si le problème persite, vous pouvez m’envoyer un courriel à <a href="mailto:ensilan[at]ensisa.info"><code>ensilan[at]ensisa.info</code></a>.</p>
+</article>

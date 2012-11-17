652d59b83113ac7b1285cb6c970bf292b2f104c6
nicolabricot <github@nicolabricot.com>
2012-11-17 11:16:44 +0100

    Ajout des erreurs
    Design à faire

---
+++ errors/403.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" />
+    <title>Erreur 403 &mdash; EnsiLAN</title>
+    <meta name="author" content="EnsiLAN" />
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 
+	<link rel="icon" type="image/png" href="/favicon.png" />
+    <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--<link rel="stylesheet" href="/css/erreurs.css" media="all" />-->
+	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW, NOARCHIVE" />
+  </head>
+
+  <body>
+	<header>
+	  <div>
+		<h1><a href="/">ensilan.com</a></h1>
+	  </div>
+	</header>
+	  
+	<article>
+	  <h2>Erreur 403 : Interdit — Forbidden</h2>
+	  <h3>Oups ! Le serveur refuse de délivrer la ressource.</h3>
+	  <p>Si le problème persite, vous pouvez m’envoyer un courriel à <a href="mailto:ensilan[at]ensisa.info"><code>ensilan[at]ensisa.info</code></a>.</p>
+	</article>
+	  
+	<footer>
+	  <nav>
+		<ul>
+		  <li><a href="/">EnsiLAN</a></li>
+		  <li><a href="/contact">Contact</a></li>
+		</ul>
+	  </nav>
+	</footer>
+	  
+</body>
+</html>

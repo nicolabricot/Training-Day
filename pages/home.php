eff1c1a4f55cc06607d36a1c2849815035be437e
nicolabricot <github@nicolabricot.com>
2012-11-17 17:42:46 +0100

    màj page home

--- pages/home.php
+++ pages/home.php
@@ -1,32 +1,22 @@
-<h2>Bienvenue sur le site d'organisation de LAN de l'ENSISA !</h2>
+<h2>Bienvenue sur le site EnsiLan :)</h2>
 
-<!--
-<section>
+<section class="aside">
     <article>
-        <h3>Accéder  l'espace membre : </h3>
-        <a href="#"><img src="/uploads/pages/home_connect.png" alt="connexion" /></a>
-        <a href="#"><img src="/uploads/pages/home_new.png" alt="connexion" /></a>
-    </article>
-</section>
--->
-<section>
-    <article>
-        <h3>EnsiLAN, c'est...</h3>
-        <p> Ce site à pour but de faciliter l'organisation des LANs au sein de l'ENSISA. Vous pourrez ainsi vous inscrire aux différentes 
-            LANs organisés durant l'année, vous inscrire aux différents tournois de jeux vidéos, réserver le matériel qui vous sera nécessaire 
-            le jour J, et bien plus encore ! Plus d'informations <a href="#">ici</a>.
+        <h3>EnsiLAN, c’est...</h3>
+        <p>Le site pour faciliter l’organisation des LANs au sein de l’ENSISA. Vous pourrez ainsi vous inscrire aux différentes 
+            LANs organisées durant l’année, vous inscrire aux différents tournois de jeux vidéos, réserver le matériel qui vous sera nécessaire 
+            le jour J, et bien plus encore !
         </p>
     </article>
-</section>
-
-<section>
     <article>
-        
+        <h3>Qui est derrière EnsiLAN ?</h3>
+        <p>EnsiLAN fait partie du XID, le club informatique de l’ENSISA. C’est un groupe de personnes qui vous veulent du bien en organiser les LANs \o/</p>
     </article>
 </section>
 
-<aside>
-    <h4 class="event">Prochain LAN le 22 novembre 2012, à partir de 19h</h4>
-    <a href="#"><img src="/uploads/pages/home_inscription.png" alt="inscription" /></a>
-    <a href="#"><img src="/uploads/pages/home_more.png" alt="connexion" /></a>
+
+<aside class="event">
+    <h3>Prochaine LAN</h3>
+    <h4>22 novembre 2012<br />8h à 19h</h4>
+    <a href="#"><img src="/uploads/pages/home_inscription_min.png" alt="inscription" /></a>
 </aside>

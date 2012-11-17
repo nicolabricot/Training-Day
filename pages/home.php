edf73d4ce5a06dd786a683538176c728831a0f1a
nicolabricot <github@nicolabricot.com>
2012-11-17 16:30:25 +0100

    Ajout de la page d'accueil

---
+++ pages/home.php
@@ -0,0 +1,24 @@
+<section>
+    <article>
+        <h2>Bienvenue sur le site d'organisation de LAN de l'ENSISA !</h2>
+        <h3>Prochain LAN le 22/11/12, à partir de 19h</h3>
+        <a href="#"><img src="inscription.png" alt="connexion" /></a>
+        <a href="#"><img src="plusInfo.png" alt="connexion" /></a>
+    </article>
+</section>
+<section>
+    <article>
+        <h3>Accéder  l'espace membre : </h3>
+        <a href="#"><img src="connect.png" alt="connexion" /></a>
+        <a href="#"><img src="new.png" alt="connexion" /></a>
+    </article>
+</section>
+<section>
+    <article>
+        <h2>EnsiLAN, c'est...</h2>
+        <p> Ce site à pour but de faciliter l'organisation des LANs au sein de l'ENSISA. Vous pourrez ainsi vous inscrire aux différentes 
+            LANs organisés durant l'année, vous inscrire aux différents tournois de jeux vidéos, réserver le matériel qui vous sera nécessaire 
+            le jour J, et bien plus encore ! Plus d'informations <a href="#">ici</a>.
+        </p>
+    </article>
+</section>
